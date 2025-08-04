88e98b63254776a2424ae07227ace4d3e3c18b26
Mohammad-Saif-JH-DAC <mdsaifcdac@gmail.com>
2025-08-04 22:35:31 +0530

    Added partial rendering of PGRooms header button

--- frontend/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Home } from './Home';
-export { default as Register } from './Register';
-export { default as RegisterForm } from './RegisterForm';
-export { default as Dashboard } from './Dashboard';
-export { default as Admin } from './Admin'; 
-export { default as ContactUs } from './ContactUs';
-export { default as MaidDashboard } from './MaidDashboard';
-export { default as UserDashboard } from './UserDashboard'; 
-
